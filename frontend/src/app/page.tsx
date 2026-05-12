4c01906c981ea33ad5cab46feee4f86e3eed1a9d
stu-account <u24616592@tuks.co.za>
2026-05-12 23:11:13 +0200

    landing page done (kinda). there's still too much

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
-import Button from "../components/ui/button";
 import Navbar from "../components/layouts/navbar";
-
+import Hero from "../components/sections/hero";
+import Image from "next/image";
 export default function Home() {
   return (
-    <Navbar/>
+    <div className="relative min-h-screen flex flex-col overflow-hidden">
+        <Image
+          src="/illustrations/hero-background.jpg"
+          alt="Background"
+          fill
+          className="absolute inset-0 object-cover grayscale brightness-25 -z-10 "
+          priority
+        />
+        <Navbar/>
+        <Hero />
+    </div>
+
   );
 }
