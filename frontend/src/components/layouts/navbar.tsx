2fd1b4b744a91195d6957a7bbdb8874627b669cd
stu-account <u24616592@tuks.co.za>
2026-05-12 22:12:38 +0200

    removed unnecessary import

--- frontend/src/components/layouts/navbar.tsx
+++ frontend/src/components/layouts/navbar.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import Button from "../ui/button";
 import {ChevronDown} from "lucide-react"
-import { link } from "fs";
 import Image from "next/image";
 
 const Navbar = () => {
