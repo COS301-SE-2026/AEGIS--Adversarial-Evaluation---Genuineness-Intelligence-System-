43c4b2cab19d66cae66ef000bfabdf178b09b6db
KutlwanoTau <u24579484@tuks.co.za>
2026-05-14 20:57:34 +0200

    Connect endpoints to the app.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 
+from app.api.routes.auth import router as auth_router
+
 app = FastAPI()
 
 # Register all API routes here
+app.include_router(auth_router, prefix="/api")
 
 
 @app.get("/")
