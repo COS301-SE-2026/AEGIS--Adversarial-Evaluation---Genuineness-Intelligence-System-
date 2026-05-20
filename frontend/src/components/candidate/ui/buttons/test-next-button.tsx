c0f56aea9d94badc348adc20148167e7ed30d42d
LuyandaNdlovu-4G <u24580717@tuks.co.za>
2026-05-20 17:18:40 +0200

    Submit button in test page has been added.

--- frontend/src/components/candidate/ui/buttons/test-next-button.tsx
+++ frontend/src/components/candidate/ui/buttons/test-next-button.tsx
@@ -1,6 +1,6 @@
 export function TestNextButton({handleNext}: {handleNext: () => void}) {
     return (
-        <button className="w-16 h-8 text-staatliches text-md bg-[#F5f5f5] text-background " onClick={handleNext}>
+        <button className="w-16 h-8 rounded-md text-staatliches tracking-wider text-md bg-default-text border border-default-text text-secondary-surface hover:bg-transparent hover:border-system-red hover:text-system-red transition-colors duration-300 cursor-pointer" onClick={handleNext}>
             <h1>Next</h1>
         </button>
     )
--- frontend/src/components/candidate/ui/buttons/test-prev-button.tsx
+++ frontend/src/components/candidate/ui/buttons/test-prev-button.tsx
@@ -1,6 +1,6 @@
 export function TestPreviousButton({handlePrevious}: {handlePrevious: () => void}) {
     return (
-        <button className=" w-16 h-8 text-staatliches text-md border border-default-border" onClick={handlePrevious}>
+        <button className=" w-16 h-8 tracking-widest rounded-md text-default-text border border-default-border hover:bg-default-text hover:text-secondary-surface hover:border-default-text transition-colors duration-300 cursor-pointer" onClick={handlePrevious}>
             <h1>Prev</h1>
         </button>
     )
