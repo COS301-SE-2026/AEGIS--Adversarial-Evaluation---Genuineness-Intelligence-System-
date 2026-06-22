2e3ef044d83e121fc205442d4aac4bf45c40ee06
KutlwanoTau <u24579484@tuks.co.za>
2026-06-22 18:58:54 +0200

    Schema model - Question creation API.

--- backend/app/schema/question.py
+++ backend/app/schema/question.py
@@ -1,5 +1,6 @@
-from typing import List, Optional
+from typing import Any, List, Optional
 from pydantic import BaseModel, Field
+from datetime import datetime
 
 
 class QuestionResponse(BaseModel):
@@ -25,6 +26,17 @@ class QuestionResponse(BaseModel):
         default_factory=list,
         description="Optional list of tags/categories",
     )
+
+class QuestionCreation(BaseModel):
+    title: str = Field(..., min_length=1, description="Question title")
+    content: str = Field(..., min_length=1, description="Question body")
+    type: str = Field(..., description="Allowed: MULTIPLE_CHOICE, TEXT,CODING")
+    maximum_score: float = Field(..., ge=0, description="Max score")
+    correct_answer: Optional[Any] = Field(None, description="Expected answer")
+    question_metadata: Optional[dict] = Field(None, description="Metadata for UI or grading")
+    tags: Optional[List[str]] = Field(default_factory=list)
+    category_id: int = Field(1, ge=1)
+    difficulty: str = Field("Easy")
 
     class Config:
         orm_mode = True
