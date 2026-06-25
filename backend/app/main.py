3085e82579a472156903f65f33983674862cb201
stu-account <u24616592@tuks.co.za>
2026-06-25 21:28:17 +0200

    fixed routers in main

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,6 +10,9 @@ from app.api.routes.user import router as user_router
 from app.api.routes.question import router as question_router2, category_router
 from app.core.config import settings
 
+from app.api.routes.question_management import router as question_router
+
+
 app = FastAPI()
 
 app.add_middleware(
@@ -25,6 +28,7 @@ app.include_router(assessment_router, prefix="/api/v1")
 app.include_router(candidate_response_router, prefix="/api/v1")
 app.include_router(user_router, prefix="/api/v1")
 app.include_router(question_router2, prefix="/api/v1")
+app.include_router(question_router, prefix="/api/v1")
 app.include_router(category_router, prefix="/api/v1")
 
 
