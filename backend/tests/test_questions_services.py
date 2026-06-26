30c7160f0775f0e17798d21ce4c4c9373de44f7f
KutlwanoTau <u24579484@tuks.co.za>
2026-06-26 13:43:01 +0200

    Added Get All question service testing.

--- backend/tests/test_questions_services.py
+++ backend/tests/test_questions_services.py
@@ -4,7 +4,7 @@ import pytest
 from fastapi import HTTPException
 
 from app.schema.question import QuestionCreation
-from app.services.question_management import create_source_question
+from app.services.question_management import create_source_question, get_all_questions
 
 def test_create_source_question_success():
     mock_db = MagicMock()
@@ -30,3 +30,19 @@ def test_create_source_question_success():
     mock_db.add.assert_called_once()
     mock_db.commit.assert_called_once()
     mock_db.refresh.assert_called_once()
+
+def test_get_all_questions_success():
+    mock_db = MagicMock()
+    mock_question = MagicMock()
+    mock_question.question_bank_id = 1
+    mock_question.title = "Python Basics"
+    mock_question.content = "What is Python?"
+    mock_question.type.value = "TEXT"
+    mock_question.maximum_score = 10
+    mock_question.tags = ["python"]
+    mock_db.query.return_value.order_by.return_value.all.return_value = [mock_question]
+    questions = get_all_questions(mock_db)
+    assert len(questions) == 1
+    assert questions[0].title == "Python Basics"
+    assert questions[0].type.value == "TEXT"
+    mock_db.query.return_value.order_by.assert_called_once()
