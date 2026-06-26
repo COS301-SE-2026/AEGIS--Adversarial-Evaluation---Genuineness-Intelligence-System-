bed941f4417485871872d95b31863b18ef418c7c
LuyandaNdlovu-4G <u24580717@tuks.co.za>
2026-06-26 12:10:08 +0200

    Altered table footer styling and added items perpage selector

--- frontend/src/app/(admin)/(question)/view-question-bank/page.tsx
+++ frontend/src/app/(admin)/(question)/view-question-bank/page.tsx
@@ -5,7 +5,7 @@ import AdminSidebar from "@/components/admin/layouts/sidebar";
 import AdminTopbar from "@/components/admin/layouts/topbar";
 import QuestionFilters from "@/components/admin/ui/input/question-filter";
 import QuestionTable from "@/components/admin/ui/cards/question-table";
-import { Plus, Menu, X} from "lucide-react";
+import { Plus } from "lucide-react";
 import { Mock_Questions, Question_Categories } from "../../types/questions";
 
 export default function ViewQuestionsPage() {
@@ -137,53 +137,75 @@ export default function ViewQuestionsPage() {
 
             />
 
-            <div className="flex flex-col sm:flex-row items-center justify-between gap-4 px-4 mt-10 border border-t-0 border-l-0 border-r-0 border-default-border rounded-b-lg text-sm text-default-text">
-              <div>
-                Showing{" "}
-                <span className="font-medium text-default-text">
-                  {questionsFiltered.length === 0 ? 0 : (currentPage - 1) * itemsPerPage + 1}
-                </span>
-                {" "} to {" "}
-                <span className="font-medium text-default-text">
-                  {Math.min(currentPage * itemsPerPage, questionsFiltered.length)}
-                </span>
-                {" "} of {" "}
-                <span className="font-medium text-default-text"></span>
-                {" "}questions
+            <div className="flex flex-col sm:flex-row items-center justify-between gap-4 px-4 py-2 mt-10 border border-t border-default-border rounded-b-lg text-sm text-default-text">
+              <div className="text-default-text order-2 sm:order-1">
+                Page <span className="font-semibold">{currentPage}</span> {" "}
+                of {" "}<span className="font-semibold">{totalNumberOfPages}</span>
               </div>
 
-              <div className="flex items-center gap-2">
+              <div className="flex items-center justify-center gap-2 sm:gap-3 order-3 sm:order-2 flex-wrap">
                 <button
                   onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
                   disabled={currentPage === 1}
-                  className="px-3 py-2 rounded border border-default-border bg-tertiary-surface hover:bg-background disabled:opacity-40 disabled:hover:bg-tertiary-surface disabled:cursor-not-allowed transition-colors text-default-text">
-                  Prev
+                  className="text-default-text text-xs sm:text-sm disabled:text-default-border px-2 py-1 rounded border border-default-border bg-tertiary-surface hover:bg-background disabled:opacity-40 disabled:hover:bg-tertiary-surface disabled:cursor-not-allowed transition-colors ">
+                  «
                 </button>
-
-                <div className="hidden sm:flex iterms-center gap-1">
-                  {Array.from({length: totalNumberOfPages}, (_, i ) => i + 1).map((page) => (
-                    <button
-                      key={page}
-                      onClick={() => setCurrentPage(page)}
-                      className={`px-3 py-2 rounded border transition-colors entry-animantion
-                        ${currentPage === page ? 
-                          "bg-system-red text-default-text" :
-                          "bg-tertiary-surface border-default-border text-default-text"
-                        }`}
-                    >
-                      {page}
-
-                    </button>
-                  ))}
-                </div>
-
                 <button 
-                  onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalNumberOfPages))}
-                  disabled={currentPage === totalNumberOfPages || totalNumberOfPages === 0}
-                  className="px-3 py-2 rounded border border-default-border bg-tertiary-surface hover:bg-background disabled:opacity-40 disabled:hover:bg-tertiary-surface disabled:cursor-not-allowed transition-colors text-default-text"
-                >
-                  Next
+                  onClick={() => setCurrentPage(Math.max(1, currentPage - 1))}
+                  disabled={currentPage === 1}
+                  className="text-default-text text-xs sm:text-sm disabled:text-default-border px-2 py-1 rounded border border-default-border bg-tertiary-surface hover:bg-background disabled:opacity-40 disabled:hover:bg-tertiary-surface disabled:cursor-not-allowed transition-colors">
+                  ‹
                 </button>
+
+                {Array.from({length: Math.min(3, totalNumberOfPages)}, (_,i) => {
+                  const pageNumber = (currentPage < 2 || currentPage === 2 ) ? i + 1 : currentPage + i - 1;
+                  return (pageNumber < totalNumberOfPages || pageNumber === totalNumberOfPages) ? pageNumber : null;
+                }).filter((pageNumber): pageNumber is number => pageNumber !== null).map((pageNumber) => (
+                  <button
+                    key={pageNumber}
+                    onClick={() => setCurrentPage(pageNumber)}
+                    className={`px-2 sm:px-3 py-1 text-xs sm:text-sm rounded transitions-colors
+                      ${currentPage === pageNumber ?
+                        "bg-default-text text-background" :
+                        "text-default-text hover:bg-tertiary-surface"
+                      }`}>
+                      {pageNumber}
+                    </button>
+                ))}
+
+                <button
+                  onClick={() => setCurrentPage(Math.min(totalNumberOfPages, currentPage + 1))}
+                  disabled={currentPage === totalNumberOfPages || totalNumberOfPages === 0}
+                  className="text-default-text text-xs sm:text-sm disabled:text-default-border px-2 py-1 rounded border border-default-border bg-tertiary-surface hover:bg-background disabled:opacity-40 disabled:hover:bg-tertiary-surface disabled:cursor-not-allowed transition-colors">
+                  ›
+                </button>
+                <button 
+                  onClick={() => setCurrentPage(totalNumberOfPages)}
+                  disabled={currentPage === totalNumberOfPages || totalNumberOfPages === 0}
+                  className="text-default-text text-xs sm:text-sm disabled:text-default-border px-2 py-1 rounded border border-default-border bg-tertiary-surface hover:bg-background disabled:opacity-40 disabled:hover:bg-tertiary-surface disabled:cursor-not-allowed transition-colors"
+                >
+                  »
+                </button>
+              </div>
+
+              <div className="flex items-center gap-2 order-1 sm:order-3">
+                <label htmlFor="items-select" className="text-default-text text-xs sm:text-sm whitespace-nowrap">
+                  Per page:
+                </label>
+                <select
+                  id="items-select"
+                  value={itemsPerPage}
+                  onChange={(event) => {
+                    setItemsPerPage(Number(event.target.value));
+                    setCurrentPage(1);
+                  }}
+                  className="px-2  bg-tertiary-surface border border-default-border rounded text-default-text text-xs sm:text-sm cursor-pointer"
+                  >
+                    <option value={8}>8</option>
+                    <option value={12}>12</option>
+                    <option value={16}>16</option>
+                    <option value={25}>25</option>
+                  </select>
               </div>
             </div>
           </div>
