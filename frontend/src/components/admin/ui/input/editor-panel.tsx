603d9e8e4e1613299dccbe616544b57c870f40b7
LuyandaNdlovu-4G <u24580717@tuks.co.za>
2026-06-28 20:48:12 +0200

    Renamed "code-editor" to "editor-panel". Added styling to "editor-panel"

--- frontend/src/components/admin/ui/input/editor-panel.tsx
+++ frontend/src/components/admin/ui/input/editor-panel.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react"
 import { Play, CheckCircle2, AlertTriangle, Loader2 } from "lucide-react"
 import Editor from "@monaco-editor/react"
-import { StringifyOptions } from "querystring"
+
 
 interface EditorPanelProps {
     content: string,
@@ -25,12 +25,12 @@ export default function EditorPanel ({
     const handleSandboxExecution = () => {
         if(!correctAnswer.trim()) {
             setTestStatus("failed");
-            setConsoleOutput("ERROR: Solution canvas cannot be vacant.")
+            setConsoleOutput("Solution canvas cannot be vacant.")
             return;
         }
 
         setTestStatus("testing");
-        setConsoleOutput("SANDBOX INITIALIZING... \n Running compiler suite aganinst test cases....")
+        setConsoleOutput("Running compiler suite aganinst test cases....")
 
         setTimeout(() => {
         //set up actual api for this
@@ -47,35 +47,35 @@ export default function EditorPanel ({
     }
 
     return (
-        <div className="">
-            <div className="">
-                <h2 className="">
+        <div className="space-y-6 h-full flex flex-col">
+            <div className="flex-1 flex flex-col bg-secondary-surface p-6 rounded-lg border border-tertiary-surface">
+                <h2 className="text-xl text-default-text tracking-wider border border-tertiary-surface pb-2 mb-4">
                     Description
                 </h2>
                 <textarea
                     value={content}
                     onChange={(event) => setContent(event.target.value)}
                     placeholder="Describe the constraints, requirements and edge cases here..."
-                    className=""
+                    className="w-full flex-1 min-h-30 p-4 bg-background border border-default-border rounded text-default-text text:sm focus:outline-none focus:border-system-red transition-colors resize-y"
                 />
             </div>
 
-            <div className="">
-                <div className="">
-                    <h2 className="">
+            <div className="flex-1 flex flex-col bg-secondary-surface p-6 rounded-lg border border-tertiary-surface">
+                <div className="flex justify-between items-center border-b border-tertiary-surface">
+                    <h2 className="text-xl text-default-text tracking-wider">
                         Solution Architecture
                     </h2>
 
                     <select
                         value={language}
                         onChange={(event) => setLanguage(event.target.value)}
-                        className=""
+                        className="font-jetbrains-mono text-[12px] bg-background border-default-border/45 text-status-info px-2 py-1 rounded uppercase focus:border-system-red cursor-pointer shadow-sm"
                     >
                         <option value="python">Python</option> {/*in future add a prop with all possible coding languages supported */}
                     </select>
                 </div>
 
-                <div className="">
+                <div className="w-full flex-1 min-h-60 rounded border border-default-border overflow-hidden bg-tertiary-surface">
                     <Editor
                         height="240px"
                         theme="vs-dark"
@@ -104,27 +104,27 @@ export default function EditorPanel ({
                     />
                 </div>
 
-                <div className="">
-                    <div className="">
-                        <div className="">
+                <div className="mt-4 pt-4 border-t border-default-border space-y-3">
+                    <div className="flex items-center justify-between">
+                        <div className="flex items-center gap-2">
                             {testStatus === "idle" && (
-                                <span className="">
-                                    <Loader2 size={14} className=""/> Untested
+                                <span className=" text-default-border uppercase font-jetbrains text-xs">
+                                    Untested
                                 </span>
                             )}
                             {testStatus === "testing" && (
                                 <span className="">
-                                    <Loader2 size={14} className=""/> Sandbox Running...
+                                    <Loader2 size={14} className="flex items-center gap-2 font-jetbrains text-xs text-status-warning animate-pulse"/> Sandbox Running...
                                 </span>
                             )}
                             {testStatus === "passed" && (
                                 <span className="">
-                                    <Loader2 size={14} className=""/> Solution Verified
+                                    <CheckCircle2 size={14} className="flex items-center gap-2 font-jetbrains text-xs text-status-success"/> Solution Verified
                                 </span>
                             )}
                             {testStatus === "failed" && (
                                 <span className="">
-                                    <Loader2 size={14} className=""/> Compilation Error
+                                    <AlertTriangle size={14} className="flex items-center gap-2 font-jetbrains text-xs text-system-red"/> Compilation Error
                                 </span>
                             )}
                         </div>
@@ -133,7 +133,7 @@ export default function EditorPanel ({
                             type="button"
                             onClick={handleSandboxExecution}
                             disabled={testStatus === "testing"}
-                            className=""
+                            className="bg-default-text text-background hover:bg-transparent hover:text-system-red hover:border-system-red  px-4 py-2 transition-colors duration-300 cursor-pointer disabled:opacity-40 disabled:cursor-not-allowed"
                         >
                             <Play size={11} fill="currentColor"/>
                             Run Test
@@ -141,8 +141,8 @@ export default function EditorPanel ({
                     </div>
 
                     {consoleOutput && (
-                        <pre className="">
-
+                        <pre className="p-3 bg-background rounded border border-tertiary-surface text-[12px] leading-relaxed text-default-border whitespace-pre-wrap max-h-30 overflow-y-auto">
+                            {consoleOutput}
                         </pre>
                     )}
                 </div>
