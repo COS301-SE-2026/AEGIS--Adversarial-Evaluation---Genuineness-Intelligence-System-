b521b04db04a706de2f4bae4c0be67b4d01094f2
KutlwanoTau <u24579484@tuks.co.za>
2026-07-05 21:39:38 +0200

    Fixing Linting Issues

--- backend/app/models/adversarial_strategies.py
+++ backend/app/models/adversarial_strategies.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, Text, DateTime, func
 from sqlalchemy.orm import relationship
 from app.models.base import Base
 
+
 class AdversarialStrategy(Base):
     __tablename__ = "adversarial_strategies"
     strategy_id = Column(Integer, primary_key=True, autoincrement=True)
