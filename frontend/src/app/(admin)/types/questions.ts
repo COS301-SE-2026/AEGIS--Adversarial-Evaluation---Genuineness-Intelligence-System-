e588ab9be8f94d33c4ad8e82f88d48a414e69bab
LuyandaNdlovu-4G <u24580717@tuks.co.za>
2026-07-07 22:28:27 +0200

    Created a dockerfile for backend deployment

--- frontend/src/app/(admin)/types/questions.ts
+++ frontend/src/app/(admin)/types/questions.ts
@@ -30,39 +30,3 @@ export interface QuestionPayload {
     created_at?: string,
     updated_at?: string
 }
-
-export const Mock_Questions: QuestionBank[] = [
-    {
-        question_bank_id: 1,
-        title: "Two Sum",
-        content: "Find two numbers that add up to a target in an array",
-        question_metadata: "Python",
-        tags: ["Array", "Hash Table"],
-        category_id: 1,
-        difficulty: "Easy"
-    },
-    {
-        question_bank_id: 2,
-        title: "Add Two Numbers",
-        content: "Add two numbers represented as linked lists in reverse order",
-        question_metadata: "Python",
-        tags: ["Linked List", "Math"],
-        category_id: 2,
-        difficulty: "Medium"
-    },
-]
-
-export const Question_Categories: QuestionCategory[] = [
-    {
-        category_id: 1,
-        category_name: "Algorithms"
-    },
-    {
-        category_id: 2,
-        category_name: "Reasoning"
-    },
-    {
-        category_id: 3,
-        category_name: "Trees"
-    }
-]
