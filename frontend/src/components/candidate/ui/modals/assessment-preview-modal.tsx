2f9ed015bc51066f722664ff6c5ea991d91c780b
LuyandaNdlovu-4G <u24580717@tuks.co.za>
2026-07-10 19:20:57 +0200

    Styled the modal

--- frontend/src/components/candidate/ui/modals/assessment-preview-modal.tsx
+++ frontend/src/components/candidate/ui/modals/assessment-preview-modal.tsx
@@ -1,5 +1,6 @@
 "use client";
-
+import { useEffect, useState } from "react"
+import { createPortal } from "react-dom" // will allow for a full screen modal
 import { AssessmentCardProps } from "../cards/assessment-card.types";
 import { StartAssessmentButton } from "../buttons/start-assessment-button";
 
@@ -18,16 +19,32 @@ const TestRules = [
 ];
 
 export function AssessmentPreviewModal({assessment, onClose,onStart}: AssessmentPreviewModalProps) {
-    return (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-background/80 backdrop-blur-sm p-4"
+    // mounting a component is used to make the modal show on screen for the first time this prevents server side rendering from Next.js
+    // the useEffect will then change this modal after it has mounted.
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+        
+        // disable background page scrolling whilst the modal is open
+        document.body.style.overflow = "hidden";
+        return () => {
+            document.body.style.overflow = "unset";
+        };
+    }, []);
+
+    if(!mounted) return null; //pre
+
+    return createPortal (
+        <div className="fixed inset-0 z-9999 flex items-center justify-center bg-background/80 backdrop-blur-sm p-4 sm:p-6"
             onClick={onClose}
         >
-            <div className="relative w-full max-h-[90vh] overflow-y-auto rounded-lg border-2 border-default-border bg-secondary-surface p-8"
+            <div className="relative w-full max-w-2xl max-h-[90vh] overflow-y-auto rounded-lg border-2 border-default-border/70 bg-background p-6 sm:p-8 shadow-2xl"
                 onClick={(event) => event.stopPropagation()}
             >
                 <button
                     onClick={onClose}
-                    className="absolute top-4 right-4 text-default-text opacity-50 hover:opacity-100 transition-opacity"
+                    className="absolute top-4 right-4 text-default-text opacity-50 hover:opacity-100 p-2 transition-opacity"
                     aria-label="Close"
                 >
                     <svg xmlns="http://www.w3.org/200/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round">
@@ -35,62 +52,89 @@ export function AssessmentPreviewModal({assessment, onClose,onStart}: Assessment
                         <line x1="6" y1="6" x2="18" y2="18"/>
                     </svg>
                 </button>
-                <div className="mb-6">
+                <div className="mb-6 shrink-0">
                 <h2 className="text-3xl text-default-text tracking-widest mb-2">
                     Assessment Details
                 </h2>
-                <div className="h-1 w-16 bg-system-red"></div>
+                <div className="shrink-0 border-t border-default-border rounded-full"></div>
             </div>
 
-            <div className="mb-8">
-                <h3 className="text-2xl text-default-text tracking-widest mb-3">
-                    {assessment.title}
-                </h3>
-                <p className="text-default-text leading-relaxed mb-6">
-                    {assessment.description}
-                </p>
+            <div className="flex-1 overflow-auto-y pr-2 space-y-6 mb-6 min-h-0">
+                <div className="space-y-2">
+                    <h3 className="text-2xl text-default-text tracking-widest mb-3">
+                        {assessment.title}
+                    </h3>
+                    <p className="text-default-text leading-relaxed mb-6">
+                        {assessment.description}
+                    </p>
+                </div>
 
-                <div className="flex flex-col sm:flex-row gap-4 border border-default-border rounded-md p-4 bg-tertiary-surface">
+                <div className="flex items-center border border-status-success/80 rounded-md p-4 w-36">
                     <div className="flex items-center flex-1">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mr-3 text-white-smoke/80">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mr-3 text-default-text mb-5">
                             <circle cx="12" cy="12" r="10"/>
                             <polyline points="12 6 12 12 16 14"/>
                         </svg>
                         <div>
                             <p>Duration</p>
-                            <p>{assessment.durationMins}</p>
+                            <p>
+                                {(() => {
+                                    const mins = assessment.durationMins;
+                                    if(mins < 60) return `${mins} min`;
+
+                                    const hours = Math.floor(mins/60);
+                                    const remainderMins = mins % 60;
+                                    
+                                    return remainderMins > 0 ?
+                                        `${hours}H ${remainderMins}min` : 
+                                        `${hours}H`
+                                })()}
+                            </p>
+                            
+                        </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-                <div className="mb-8">
-                    <h4>
+
+                <div className="space-y-3">
+                    <h4 className="text-xs uppercase tracking-wider foont-medium text-default-text">
                         Test Rules &amp; Instructions
                     </h4>
                     <ul className="space-y-3">
                         {TestRules.map((rule, index) => (
-                            <li key={index} className="flex items-start text-sm text-default-text">
+                            <li key={index} className="flex items-start text-sm text-default-text leading-relaxed">
                                 <span className="mr-3 mt-0.5"><b>-</b></span>
-                                <span>{rule}</span>
+                                <span className="wrap-break-words">{rule}</span>
                             </li>
                         ))}
                     </ul>
-                    <span className="font-staatliches text-sm"><b>By clicking "Start Test", you acknowledge that you have read, understood, and agree to abide by these terms and conditions</b></span>
+
                 </div>
 
-                <div className="flex flex-col sm:flex-row gap-4 justify-end mt-8 border-default-border pt-6">
-                    <button
-                        onClick={onClose}
-                        className="h-12 px-6 rounded-md border-2 border-default-border text-default-text hover:bg-tertiary-surface transition-colors duration-300 cursor-pointer"
-                    >
-                        <h3 className="tracking-widest">Cancel</h3>
-                    </button>
-                    <StartAssessmentButton
-                        onClick={onStart}
-                    />
+                <div className="shrink-0 border-t border-default-border mt-4 pt-4">
+                    <span className="text-sm text-default-text italic mb-4 leading-normal">
+                        By clicking "Start", you acknowledge that you have read, understood, and agree to abide by these terms and conditions
+                    </span>
+                    
+                    <div className="flex flex-col-reverse sm:flex-row gap-3 justify-end items-center">
+                        <button
+                            onClick={onClose}
+                            className="flex items-center justify-center w-full sm:w-auto h-11 px-6 rounded-md border-2 border-default-border text-default-text hover:bg-tertiary-surface font-staatliches font-medium sm:text-base tracking-wider transition-colors duration-300 cursor-pointer"
+                        >
+                            Cancel
+                        </button>
+
+                        <div className="w-full sm:w-auto flex justify-center sm:block sm:-mt-4">
+                            <StartAssessmentButton
+                                onClick={onStart}
+                            />
+                        </div>
+                        
+                    </div>
                 </div>
             </div>
-        </div>
+        </div>,
+        document.body // this will put this element cleanly into the apps root layer
     )
 }
