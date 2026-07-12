06f481e5a418bb3cf0fce546f0e3bea507e67310
KutlwanoTau <u24579484@tuks.co.za>
2026-07-12 13:49:47 +0200

    Get Adversarial Question Service

--- backend/app/services/test_cases.py
+++ backend/app/services/test_cases.py
@@ -1,7 +1,9 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status
+from app.models.adversarial_question import AdversarialQuestion
 from app.models.coding_test_cases import CodingTestCase
 from app.models.question_bank import QuestionBank
+from app.schema.test_cases import CodingTestCaseCreate, CodingTestCaseUpdate
 
 
 def get_test_cases_by_question_id(
@@ -21,3 +23,19 @@ def get_test_cases_by_question_id(
         .filter(CodingTestCase.question_id == question_id)
         .order_by(CodingTestCase.test_case_id)
         .all())
+
+def get_adversarial_question(
+    db: Session,
+    adv_question_id: int,
+) -> AdversarialQuestion:
+    adversarial_question = (
+        db.query(AdversarialQuestion)
+        .filter(AdversarialQuestion.adv_question_id == adv_question_id)
+        .first()
+    )
+    if adversarial_question is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Adversarial question not found",
+        )
+    return adversarial_question
