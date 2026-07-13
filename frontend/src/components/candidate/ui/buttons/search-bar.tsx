5e474fb82a99aa98c0b0491a3c4c379040b7f9cc
LuyandaNdlovu-4G <u24580717@tuks.co.za>
2026-07-13 13:09:21 +0200

    added states to keep the lastest search param

--- frontend/src/components/candidate/ui/buttons/search-bar.tsx
+++ frontend/src/components/candidate/ui/buttons/search-bar.tsx
@@ -1,7 +1,8 @@
 
 "use client"
 import Image from "next/image";
-import { useRouter,useSearchParams, usePathname } from "next/navigation"
+import { useRouter ,useSearchParams, usePathname } from "next/navigation";
+import { useState, useEffect } from "react";
 
 
 export function SearchBar() {
@@ -9,20 +10,30 @@ export function SearchBar() {
     const pathname = usePathname();
     const searchParams = useSearchParams();
 
-    const searchQuery = searchParams.get("search") ?? "";
+    const [localQuery, setLocalQuery] = useState(searchParams.get("search") ?? "");
 
-    const handleSearch = (element: React.ChangeEvent<HTMLInputElement>) => {
-        const value = element.target.value;
-        const params = new URLSearchParams(searchParams.toString());
-        
-        if(value) {
-            params.set("search", value);
-        }
-        else {
-            params.delete("seach");
-        }
-        router.replace(`${pathname}?${params.toString()}`);
-    }
+    useEffect(() => {
+        setLocalQuery(searchParams.get("search") ?? "");
+    }, [searchParams]);
+
+    useEffect(() => {
+        const rateLimit = setTimeout(() => {
+            const params = new URLSearchParams(searchParams.toString());
+
+            if(localQuery){
+                params.set("search", localQuery);
+            }
+            else {
+                params.delete("search");
+            }
+
+            router.replace(`${pathname}?${params.toString()}`, {scroll: false});
+        }, 300)
+
+        return () => clearTimeout(rateLimit);
+    }, [localQuery, pathname, router, searchParams])
+
+    const searchQuery = searchParams.get("search") ?? "";
 
     return (
         <div className="flex items-center">
@@ -30,9 +41,9 @@ export function SearchBar() {
                 <Image src="/illustrations/icons/search-icon.svg" alt="Search Icon" width={20} height={20} className="absolute left-3 pointer-events-none" />
                 <input
                     type="text"
-                    placeholder="Search assessments..."
-                    value={searchQuery}
-                    onChange={handleSearch}
+                    placeholder="Search..."
+                    value={localQuery}
+                    onChange={(event) => setLocalQuery(event.target.value)}
                     className="w-40  h-[36] pl-10 pr-4 py-2 border  border-default-border/75 rounded-md focus:outline-none focus:ring-2 focus:ring-white-smoke transition duration-200"
                 />
             </div>
