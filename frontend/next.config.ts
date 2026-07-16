54adc1f446fe538afbd8f275a9eddee320a5d133
MyronWheeler <u24579565@tuks.co.za>
2026-07-16 13:14:07 +0200

    Fix, Lusanda Vercel patch

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  outputFileTracingRoot: path.join(__dirname, "../../"),
+  outputFileTracingRoot: path.resolve(__dirname, ".."),
 };
 
 export default nextConfig;
