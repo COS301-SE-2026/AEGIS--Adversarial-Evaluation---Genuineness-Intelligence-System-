4614c83d01d287394e20154b49e0cf3f42a1e74f
stu-account <u24616592@tuks.co.za>
2026-07-30 18:30:55 +0200

    comment out for sonar purposes

--- frontend/src/components/hero/layouts/navbar.tsx
+++ frontend/src/components/hero/layouts/navbar.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Button from "../ui/button";
-import {ChevronDown} from "lucide-react"
+// import {ChevronDown} from "lucide-react"
 import Image from "next/image";
 
 const Navbar = () => {
